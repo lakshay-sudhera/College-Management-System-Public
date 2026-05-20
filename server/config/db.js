22c48a2fb635015bcc7ffbc25aede328c7a38aa7
Lakshay Sudhera <lakshaysudhera309@gmail.com>
2026-05-20 22:02:54 +0530

    fixed mongodb uri issue

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const DB_NAME=require("../constants")
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    const conn = await mongoose.connect(`${process.env.MONGO_URI}`);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection error !",error);
